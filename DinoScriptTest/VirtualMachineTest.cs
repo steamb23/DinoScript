93096a2abe8436952142ab86ebe8e49ae4b26247
SteamB23 <steamb23@outlook.com>
2022-04-24 03:29:54 +0900

    가상 머신 테스트 추가

--- DinoScriptTest/VirtualMachineTest.cs
+++ DinoScriptTest/VirtualMachineTest.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using DinoScript.Code;
 using DinoScript.Parser;
 using DinoScript.Runtime;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace DinoScript.Test;
 
@@ -11,29 +13,48 @@ using static InternalCode;
 
 public class VirtualMachineTest
 {
+    private readonly ITestOutputHelper testOutputHelper;
+
+    public VirtualMachineTest(ITestOutputHelper testOutputHelper)
+    {
+        this.testOutputHelper = testOutputHelper;
+    }
+
     public static IEnumerable<object[]> ExpressionTestDataList => new[]
     {
         new object[]
         {
             "1 + 2 * 3 + 4",
-            11
+            1 + 2 * 3 + 4
         },
+        new object[]
+        {
+            "10*0+10",
+            10 * 0 + 10
+        },
+        new object[]
+        {
+            "1.0 / 3.0",
+            1.0 / 3.0
+        }
     };
-    
+
     [Theory]
     [MemberData(nameof(ExpressionTestDataList))]
-    public void ExpressionTest(string text, int expectedValue)
+    public void ExpressionTest(string text, double expectedValue)
     {
         var vm = new VirtualMachine(new StringReader(text), new()
         {
             ParserMode = ParserMode.ExpressionTest,
         });
-        
+
         vm.Run();
-        
+
         // 결과값 가져오기
         var result = vm.Memory.Stack.PeekDouble();
-        
-        Assert.Equal(expectedValue, result);
+
+        testOutputHelper.WriteLine($"expectedValue: {expectedValue}, actualValue: {result}");
+
+        Assert.Equal(expectedValue, result, 8);
     }
 }
