209b67d9edd6a9c4cc7757aa61b5598cb2f1f462
SteamB23 <steamb23@outlook.com>
2022-04-24 02:51:23 +0900

    가상 머신 테스트 추가

---
+++ DinoScriptTest/VirtualMachineTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using DinoScript.Code;
+using DinoScript.Parser;
+using DinoScript.Runtime;
+using Xunit;
+
+namespace DinoScript.Test;
+
+using static InternalCode;
+
+public class VirtualMachineTest
+{
+    public static IEnumerable<object[]> ExpressionTestDataList => new[]
+    {
+        new object[]
+        {
+            "1 + 2 * 3 + 4",
+            11
+        },
+    };
+    
+    [Theory]
+    [MemberData(nameof(ExpressionTestDataList))]
+    public void ExpressionTest(string text, int expectedValue)
+    {
+        var vm = new VirtualMachine(new StringReader(text), new()
+        {
+            ParserMode = ParserMode.ExpressionTest,
+        });
+        
+        vm.Run();
+        
+        // 결과값 가져오기
+        var result = vm.Memory.Stack.PeekDouble();
+        
+        Assert.Equal(expectedValue, result);
+    }
+}
