c97339a754eaad21b70efffb7be4082603a529f3
SteamB23 <steamb23@outlook.com>
2022-04-25 02:25:28 +0900

    Script.Run 옵션 추가

--- DinoScript/Script.cs
+++ DinoScript/Script.cs
@@ -2,16 +2,16 @@
 
 namespace DinoScript;
 
-public class DinoScript
+public class Script
 {
     /// <summary>
     /// 실행 후 가상 머신을 가져옵니다.
     /// </summary>
     /// <param name="script"></param>
     /// <returns></returns>
-    public static VirtualMachine Run(string script)
+    public static VirtualMachine Run(string script, VirtualMachineOptions options = null)
     {
-        var virtualMachine = new VirtualMachine(new StringReader(script));
+        var virtualMachine = new VirtualMachine(new StringReader(script), options);
         virtualMachine.Run();
         return virtualMachine;
     }
