657cb93a98b887c7c7cc58c3df79f7f22726b379
SteamB23 <steamb23@outlook.com>
2022-05-30 22:01:40 +0900

    StatementTest 추가

--- DinoScriptTest/ParserTest_StatementTest.cs
+++ DinoScriptTest/ParserTest_StatementTest.cs
@@ -21,6 +21,18 @@ public partial class ParserTest
     {
         new object[]
         {
+            "let a = 10\n" +
+            "a = 20",
+            new[]
+            {
+                Make(Opcode.LoadConstantInteger, null, (long)10),
+                Make(Opcode.StoreToNewLocal, null),
+                Make(Opcode.LoadConstantInteger, null, (long)20),
+                Make(Opcode.StoreToLocal, null, 0),
+            }
+        },
+        new object[]
+        {
             "let a = 10; a = 20;",
             new[]
             {
@@ -32,7 +44,9 @@ public partial class ParserTest
         },
         new object[]
         {
-            "let a = 10; let b = 20; a = b;",
+            "let a = 10\n" +
+            "let b = 20\n" +
+            "a = b",
             new[]
             {
                 Make(Opcode.LoadConstantInteger, null, (long)10),
